656d9a937e26edab29da8974b17186bbd85171c1
20061920 <20061920@student.westernsydney.edu.au>
2026-09-22 19:15:47 +1000

    fixed testing issue

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 
-// @ts-expect-error: side-effect import for global CSS
+
 import "./globals.css";
 
 const roboto = Roboto({
